21d08d346dc36c432b3e31fc03619975dcc58a76
Djirgal <djirgal.nemgirova@gmail.com>
2022-12-15 00:07:59 +0300

    Zadacha 4 was added

--- Seminar_1/Seminar_1_DZ.py
+++ Seminar_1/Seminar_1_DZ.py
@@ -9,3 +9,13 @@ sum = 0
 for i in range(len(chislo)):
     sum += int(chislo[i])
 print('The result is ', sum)
+
+'''Задача 4
+Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов. 
+Сколько журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов,
+ а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?'''
+print('Please, enter the number of  cranes -> ', end='')
+total = int(input())
+petya  = total / 6
+katya = 2 * 2 * petya
+print('The result is  {} + {} + {}'.format(petya, katya, petya))
