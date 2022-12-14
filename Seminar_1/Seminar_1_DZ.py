5c8780172c9f699f93177fae295dae7dfa35d306
Djirgal <djirgal.nemgirova@gmail.com>
2022-12-14 23:34:52 +0300

    Zadacha 2 was enriched with the text in console

--- Seminar_1/Seminar_1_DZ.py
+++ Seminar_1/Seminar_1_DZ.py
@@ -3,9 +3,9 @@
 Пример:
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0)'''
+print('Please, enter any number -> ', end='')
 chislo = str(input())
 sum = 0
 for i in range(len(chislo)):
     sum += int(chislo[i])
-print(sum)
-
+print('The result is ', sum)
