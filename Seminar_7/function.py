5d785d6661cb00b462e05d11db32714dadec4d80
Djirgal <djirgal.poshtarova@yandex.ru>
2023-02-02 01:20:54 +0300

    Продублированы действия из 7-го семинара

---
+++ Seminar_7/function.py
@@ -0,0 +1,21 @@
+def convert():
+    return
+
+def print_bus():
+
+    return
+
+def add_bus():
+    return
+
+def print_driver():
+    return
+
+def add_driver():
+    return
+
+def print_route():
+    return
+
+def add_route():
+    return
